a5385160ee299abf0bb9e1b49fc6f19a97244a18
marioschmidt4 <mario.schmidt4@gmx.net>
2017-11-13 22:40:16 +0100

    progress 155 (about_triangle_project)

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if (a == b && a == c)
+    return :equilateral
+  elsif (a == b && a != c)
+    return :isosceles
+  elsif (a != b && a == c)
+    return :isosceles
+  elsif (c == b && a != c)
+    return :isosceles
+  else
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
